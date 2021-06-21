14dc2aa27062e6aa78998ba0c6aea6a05a23d61e
Ngô Đăng Hải <haind3@sapo.vn>
2021-06-21 11:50:40 +0700

    fix bcryptEncoder

--- src/main/java/vn/actvn/onthionline/config/WebSecurityConfig.java
+++ src/main/java/vn/actvn/onthionline/config/WebSecurityConfig.java
@@ -39,11 +39,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // configure AuthenticationManager so that it knows from where to load
         // user for matching credentials
         // Use BCryptPasswordEncoder
-        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder bcryptEncoder() {
         return new BCryptPasswordEncoder();
     }
 
